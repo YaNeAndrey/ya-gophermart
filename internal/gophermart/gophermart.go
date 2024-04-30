bff134535f59061c8cbad830f317ce42dea98a0d
YaNeAndrey <andrey.bery@yandex.ru>
2024-04-30 08:35:10 -0700

    Bugs

--- internal/gophermart/gophermart.go
+++ internal/gophermart/gophermart.go
@@ -6,29 +6,12 @@ import (
 	"github.com/YaNeAndrey/ya-gophermart/internal/gophermart/router"
 	"github.com/YaNeAndrey/ya-gophermart/internal/gophermart/storage"
 	"net/http"
-	"time"
 )
 
 func InitGophermart() {
 	conf := parser.ParseConfig()
 	st := parser.ParseStorageInfo()
 	orderCh := make(chan storage.Order, 100)
-	orderCh <- storage.Order{
-		Number:        1,
-		Status:        "22",
-		Accrual:       0,
-		UploadDate:    time.Time{},
-		Sum:           0,
-		ProcessedDate: time.Time{},
-	}
-	orderCh <- storage.Order{
-		Number:        5,
-		Status:        "22",
-		Accrual:       0,
-		UploadDate:    time.Time{},
-		Sum:           0,
-		ProcessedDate: time.Time{},
-	}
 	am := accrualmanager.InitAccrualManager(conf, st, orderCh)
 	am.Start()
 
