58f63a2c8abbd71a9ba9da2e847604c6f3efb773
YaNeAndrey <andrey.bery@yandex.ru>
2024-04-30 06:18:19 -0700

    .

--- internal/gophermart/router/router.go
+++ internal/gophermart/router/router.go
@@ -22,17 +22,18 @@ func InitRouter(st *storage.Storage, ordersCh chan storage.Order) http.Handler {
 	r.Use(middleware.Gzip())
 
 	r.Route("/", func(r chi.Router) {
-		r.Post("/api/user/register/", func(rw http.ResponseWriter, req *http.Request) {
+		r.Post("/api/user/register", func(rw http.ResponseWriter, req *http.Request) {
 			handler.RegistrationPOST(rw, req, st)
 		})
-		r.Post("/api/user/login/", func(rw http.ResponseWriter, req *http.Request) {
+		r.Post("/api/user/login", func(rw http.ResponseWriter, req *http.Request) {
 			handler.LoginPOST(rw, req, st)
 		})
 
 		//after authorization
 		r.Route("/api/user/", func(r chi.Router) {
+
 			//	r.Use(middleware.CheckAccess())
-			r.Route("/orders/", func(r chi.Router) {
+			r.Route("/orders", func(r chi.Router) {
 				r.Post("/", func(rw http.ResponseWriter, req *http.Request) {
 					handler.OrdersPOST(rw, req, st, ordersCh)
 				})
